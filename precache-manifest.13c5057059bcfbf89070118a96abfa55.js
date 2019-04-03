f98ee45f361568575b643111e9a2894b424bcab1
Rick <654834341@qq.com>
2019-04-03 14:14:16 -0500

    Updates

---
+++ precache-manifest.13c5057059bcfbf89070118a96abfa55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rss-pwa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ef7b4f373985221a9ead",
+    "url": "/rss-pwa/static/js/runtime~main.d7ebab49.js"
+  },
+  {
+    "revision": "e692473e89a7b19cb943",
+    "url": "/rss-pwa/static/js/main.0299e051.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/rss-pwa/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e692473e89a7b19cb943",
+    "url": "/rss-pwa/static/css/main.4fb1b803.chunk.css"
+  },
+  {
+    "revision": "fd6f378fb6b3238afbc1fcc33febecf0",
+    "url": "/rss-pwa/index.html"
+  }
+];
