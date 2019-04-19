ebd74f09a1ba0c9e44dd0a352f4f40087cacba89
beijingjack <lijingyu951126@gmail.com>
2019-04-18 23:00:28 -0500

    Modified detail

--- src/News/components/NewsDetail.jsx
+++ src/News/components/NewsDetail.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../styles/NewsDetail.scss';
-import { Button, Empty, Tag} from 'antd';
+import { Button, Empty, Tag, Modal} from 'antd';
 import news_list from "../../mock/news_list"
 
 class NewsDetail extends Component {
@@ -8,14 +8,40 @@ class NewsDetail extends Component {
     super();
     this.state = {
       currentNews: {},
+      subscribed: false,
+      visible: false
     }
   }
 
   componentDidMount() {
     this.setState({currentNews: news_list[this.props.match.params.id - 1]});
+    // set subscribed
   }
 
+  handleClick = () => {
+    const currState = this.state.subscribed;
+    if (currState) {this.showModal()}
+    else {this.setState({subscribed: true})}
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    })
+  };
+
+  handleOk = () => {
+    this.setState({subscribed: false})
+    this.setState({ visible: false });
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
   render() {
+    const type = this.state.subscribed ? "default" : "primary";
+    const value = this.state.subscribed ? "Subscribed" : "Subscribe";
     const tempDate = new Date();
     const {currentNews} = this.state;
     if (!currentNews) {
@@ -33,8 +59,21 @@ class NewsDetail extends Component {
           <div className="post-time">{tempDate.toString()}</div>
           <div className="media">
             <div className="source">Source: {currentNews.source}</div>
-            <div><Button type="primary" size="small">Subscribe</Button></div>
+            <div><Button type={type} size="small" onClick={this.handleClick}>{value}</Button></div>
           </div>
+          <Modal
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+            footer={[
+              <Button key="Cancel" onClick={this.handleCancel}>Cancel</Button>,
+              <Button key="Unsubscribe" type="primary" onClick={this.handleOk}>
+                Unsubscribe
+              </Button>,
+            ]}
+          >
+            <p>Are you sure to Unsubscribe from {currentNews.source}</p>
+          </Modal>
         </div>
 
         <br/>
