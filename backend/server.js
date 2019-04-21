2eaac7108f90aa9bda87a3f64f7db1252836c44e
Ramsey <qiaokun@bupt.edu.cn>
2019-04-21 16:30:45 -0500

    resend server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,26 @@
-// Get the packages we need
-var express = require('express'),
-    router = express.Router(),
-    mongoose = require('mongoose'),
-    secrets = require('./config/secrets'),
-    bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
 
-// Create our Express application
-var app = express();
-
-// Use environment defined port or 4000
-var port = process.env.PORT || 4000;
-
-// Connect to a MongoDB
-mongoose.connect(secrets.mongo_connection,  { useNewUrlParser: true });
-
-// Allow CORS so that backend and frontend could be put on different servers
-var allowCrossDomain = function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-    next();
-};
-app.use(allowCrossDomain);
-
-// Use the body-parser package in our application
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-// Use routes as a module (see index.js)
-require('./routes')(app, router);
+app.use(express.json());
+app.use(express.urlencoded());
 
-// Start the server
-app.listen(port);
-console.log('Server running on port ' + port);
+var loaddata = require("./loaddata");
+var offline = require("./offline");
+var search = require("./search");
+
+app.use("/loaddata", loaddata);
+app.use("/offline", offline);
+app.use("/search", search);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}...`);
+});
