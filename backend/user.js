0238eab13732af941c3d14ccd8818f16cc0db989
kevinhhq <30514205+kevinhhq@users.noreply.github.com>
2019-04-23 19:52:49 -0500

    User js
    path:localhost:5000/user/:id/channel
    get: /user(all the users' profile),
     /user/:id/(one user's profile)
    /user/:id/channel(one user's favorate media)
    To update channel:
    e.g.: put {"channel":"cnn"} to user/1
    if the value of channel of userid 1 does not contain cnn. add cnn to current channel
    else delete it from current channel

---
+++ backend/user.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const router = express.Router();
+const db = require("./firebase");
+
+router.get("/", function(req, res) {
+    var userReference = db.ref("/user");
+    userReference.on(
+        "value",
+        function(snapshot) {
+            res.json(snapshot.val());
+            userReference.off("value");
+        },
+        function(errorObject) {
+            console.log("The read failed: " + errorObject.code);
+            res.send("The read failed: " + errorObject.code);
+        }
+    );
+});
+
+router.get("/:id/channel", function(req, res) {
+    var id = req.params.id;
+    var userReference = db.ref("/user/"+id + "/channel");
+    userReference.on(
+        "value",
+        function(snapshot) {
+            res.json(snapshot.val());
+            userReference.off("value");
+        },
+        function(errorObject) {
+            console.log("The read failed: " + errorObject.code);
+            res.send("The read failed: " + errorObject.code);
+        }
+    );
+});
+
+router.put("/:id", function(req, res) {
+    var channel=JSON.stringify(req.body.channel).replace(/\"/g, "");
+    var id=req.params.id;
+    var CurrentData=db.ref("/user/"+id + "/channel");
+    CurrentData.once(
+        "value",
+        function(snapshot) {
+            var currentdata=snapshot.val();
+            var list=currentdata.split(',');
+            var flag=0;
+            if(list===null||list[0]===currentdata){
+                if (list[0] === channel) {
+                    list.splice(0, 1);
+                }
+                else
+                    list.splice(0,0,channel);
+            }
+            else {
+                n=list.length
+                for (var i = 0; i < n; i++)
+                    if (list[i] === channel) {
+                        list.splice(i, 1);
+                        flag = 1;
+                        break;
+                    }
+                if(flag===0){
+                    list.splice(0,0,channel);
+                }
+            }
+            console.log(list[0],list[1],list[2])
+            db.ref("/user/"+id).update({
+                channel: list.toString(),
+            })
+            res.send("Update Done");
+            CurrentData.off("value");
+        },
+        function(errorObject) {
+            console.log("The read failed: " + errorObject.code);
+            res.send("The read failed: " + errorObject.code);
+        }
+    );
+});
+
+module.exports = router;
