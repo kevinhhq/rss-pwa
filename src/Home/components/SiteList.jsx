14bcd804d6572a3bda2a187c34fc0cb32307fc6e
Rick <654834341@qq.com>
2019-04-28 14:05:45 -0500

    Finish home source list

--- src/Home/components/SiteList.jsx
+++ src/Home/components/SiteList.jsx
@@ -1,46 +1,36 @@
 import React, { Component } from 'react';
 import '../styles/SiteList.scss';
-import {Button, Card, Skeleton, Menu, Dropdown, message, Icon} from 'antd';
+import {Button, Card, Skeleton} from 'antd';
 import media_list from '../../mock/media_list.json';
+import {Link} from "react-router-dom";
 
 class SiteList extends Component {
     state = {
         mockData: media_list,
     };
 
-    onClick = ({ key }) => {
-        message.info(`Click on item ${key}`);
-    };
-
-    menu = () => (
-        <Menu onClick={this.onClick}>
-            <Menu.Item key="1"><Icon type="plus" />Follow</Menu.Item>
-            <Menu.Item key="2"><Icon type="link" />View the site</Menu.Item>
-        </Menu>
-    );
-
     render() {
         return(
             <div className="media-card-list">
                 {this.state.mockData.length === 0 ?
                     <Skeleton/>
-                    : this.state.mockData.map(news => <Card
-                        key={news.title}
+                    : this.state.mockData.map(source => <Card
+                        key={source.title}
                         hoverable
                         bordered={false}
                         style={{ width: 240 }}
-                        cover={<img alt="example" src={news.url} />}
+                        cover={<Link to={`news/?source=${source.title.toLowerCase()}`}>
+                            <img alt="example" src={source.url} style={{width: "100%"}}/>
+                        </Link>}
                     >
                         <div>
                             <h3>
-                                {news.title}
-                                <Dropdown overlay={this.menu} trigger={['click']} className="site-list-menu">
-                                    <Button icon="more" shape="circle"/>
-                                </Dropdown>
+                                {source.title}
+                                <Button icon="link" shape="circle" className="site-list-menu" href={source.source}/>
                             </h3>
                         </div>
                         <div>
-                            sample description
+                            {source.description}
                         </div>
                     </Card>)}
             </div>
