a39e9f316e845bb315628805c0f0a30bae4edf60
haoqihu <haoqihu2@illinois.edu>
2019-04-28 15:56:07 -0500

    Update user.js
    correct the user

--- backend/user.js
+++ backend/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("./firebase");
 var admin = require('firebase-admin');
+var firebase=require('firebase');
 
 router.post("/register", function(req, res) {
     admin.auth().createUser({
@@ -17,7 +18,6 @@ router.post("/register", function(req, res) {
         }
         let userRef = db.ref('user/');
         userRef.push().set(user);
-        res.redirect('/user/login');
         console.log("Successfully created new user:", userRecord.uid);
     })
         .catch(function (error) {
@@ -52,40 +52,56 @@ router.get("/:id", function(req, res) {
 
 });
 
-
-
-router.put("/:id", function(req, res) {
-    var name=JSON.stringify(req.body.name).replace(/\"/g, "");
-    var type=JSON.stringify(req.body.type).replace(/\"/g, "");
-    console.log(name,type);
+router.put("/:id/news", function(req, res) {
     var uid=req.params.id;
+    var img_url=JSON.stringify(req.body.img_url).replace(/\"/g, "");
+    var summary=JSON.stringify(req.body.summary).replace(/\"/g, "");
+    var news_id=JSON.stringify(req.body.news_id).replace(/\"/g, "");
+    var timestamp=new Date().getTime();
     var userReference = db.ref("/user");
     userReference.orderByChild("uid").equalTo(uid).on(
-        "child_added",
+        "value",
         function(snapshot) {
+            var key_id=snapshot.key;
+            var recentread=snapshot.val().news;
 
-            var key_id=snapshot.key
-            if(snapshot.hasChild("channel")){
-                var currentdata=snapshot.val().channel;
+            if(!snapshot.child("news").hasChild("recentread")){
+                recentread = {};
+                recentread[news_id]= {"img_url":img_url,"summary":summary,"timestamp":timestamp};
+            }
+            else{
                 var flag=0;
-                console.log(currentdata)
-                for(let i in currentdata){
-                    console.log(currentdata)
-                    if(currentdata[i].name===name&&currentdata[i].type===type){
-                        delete (currentdata[i])
+                var count=0;
+                for(let i in recentread){
+                    count=count+1;
+                }
+                var least_timestamp=new Date().getTime();
+                var not_recent_id=0
+                for(let i in recentread){
+                    if(i===news_id){
+                        recentread[i].timestamp=timestamp;
                         flag=1;
                         break;
                     }
+                    if(recentread[i].timestamp<least_timestamp){
+                        not_recent_id=i;
+                        least_timestamp=recentread[i].timestamp;
+                    }
+                }
+                if(count===4){
+                    recentread[not_recent_id]=({"img_url":img_url,"summary":summary,"timestamp":timestamp});
+                    flag=1;
                 }
                 if(flag===0){
-                    currentdata[name]=({"name":name,"type":type});
+                    recentread[news_id]=({"img_url":img_url,"summary":summary,"timestamp":timestamp});
+                    console.log(recentread)
                 }
             }
-            db.ref("/user/"+key_id).update({
-                channel: currentdata,
+            db.ref("/user/"+key_id+"/news").update({
+                recentread: recentread,
             })
             res.send("Update Done");
-            userReference.off("child_added");
+            userReference.off("value");
         },
         function(errorObject) {
             console.log("The read failed: " + errorObject.code);
@@ -94,4 +110,43 @@ router.put("/:id", function(req, res) {
     );
 });
 
+
+router.put("/:id", function(req, res) {
+    var name=JSON.stringify(req.body.name).replace(/\"/g, "");
+    var type=JSON.stringify(req.body.type).replace(/\"/g, "");
+    var uid=req.params.id;
+    var userReference = db.ref("/user");
+    userReference.orderByChild("uid").equalTo(uid).on(
+        "child_added",
+        function(snapshot) {
+            var key_id=snapshot.key;
+            var currentdata=snapshot.val().channel;
+            if (!snapshot.hasChild("channel")) {
+                currentdata = {};
+                currentdata[name]= {"type": type};
+            } else {
+                var flag = 0;
+                for (const i in currentdata) {
+                    if (i === name && currentdata[i].type === type) {
+                        delete (currentdata[i]);
+                        flag = 1;
+                        break;
+                    }
+            }
+            if (flag === 0) {
+                currentdata[name] = {"type": type};
+            }
+        }
+        db.ref("/user/"+key_id).update({
+            channel: currentdata,
+        })
+        res.send("Update Done");
+        userReference.off("child_added");
+    },
+    function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        res.send("The read failed: " + errorObject.code);
+    }
+);
+});
 module.exports = router;
